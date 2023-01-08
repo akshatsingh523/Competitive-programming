223655b12d479b16767899ce8f435872eabb5147
akshatsingh523 <49590899+akshatsingh523@users.noreply.github.com>
2023-01-08 20:00:16 +0530

    Create Water_consumption.cpp

---
+++ Water_consumption.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	int x;
+	for(int i=0;i<t;i++){
+	    cin>>x;
+	    if(x<2000){
+    	cout<<"NO"<<endl;
+        }
+        else{
+        cout<<"YES"<<endl;
+        }
+	}
+	
+// 	for(int i=0;i<t;i++){
+//     	if(x<=2000){
+//     	cout<<"NO"<<endl;
+//         }
+//         else{
+//         cout<<"YES"<<endl;
+//         }
+	    
+// 	}
+	return 0;
+}
