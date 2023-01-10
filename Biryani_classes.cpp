50ed28a9b9af90511d73c883a36f7ddf858811b0
Akshat Singh <49590899+akshatsingh523@users.noreply.github.com>
2023-01-11 01:47:58 +0530

    Create Biryani_classes.cpp

---
+++ Biryani_classes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	int x,y;
+	for(int i=0;i<t;i++){
+	    cin>>x;
+	    cin>>y;
+	    cout<<x*y<<endl;
+	}
+	return 0;
+}
